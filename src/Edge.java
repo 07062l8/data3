586994735bf31557b8a1f54d412b99e55d1a42f3
asima <asimaabdikarimoova6364@gmail.com>
2025-10-26 21:46:35 +0500

    grapgh

---
+++ src/Edge.java
@@ -0,0 +1,12 @@
+public class Edge {
+    public String from;
+    public String to;
+    public int weight;
+
+    public Edge(String from, String to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+}
+
